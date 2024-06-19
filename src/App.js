373e459a3e5dfe4d8ce6c788dc019abf7a97f721
gosiagrajewska <149269502+gosiagrajewska@users.noreply.github.com>
2024-06-19 17:39:56 +0200

    footer added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      Weather app
+      <p>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/gosiagrajewska"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Malgorzata Grajewska
+        </a>{" "}
+        and is open sourced on{" "}
+        <a
+          href="https://github.com/gosiagrajewska/weatherapp-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://react-weatherapp-bygosia.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </p>
+    </div>
+  );
 }
