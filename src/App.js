6283640744964e200576323bc398445ae38a41a0
gosiagrajewska <149269502+gosiagrajewska@users.noreply.github.com>
2024-06-20 15:24:13 +0200

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -3,33 +3,36 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <p>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/gosiagrajewska"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Malgorzata Grajewska
-        </a>{" "}
-        and is open sourced on{" "}
-        <a
-          href="https://github.com/gosiagrajewska/weatherapp-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>{" "}
-        and hosted on{" "}
-        <a
-          href="https://react-weatherapp-bygosia.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </p>
+      <div className="container">
+        <h1>Weather app</h1>
+
+        <p>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/gosiagrajewska"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Malgorzata Grajewska
+          </a>{" "}
+          and is open sourced on{" "}
+          <a
+            href="https://github.com/gosiagrajewska/weatherapp-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://react-weatherapp-bygosia.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
